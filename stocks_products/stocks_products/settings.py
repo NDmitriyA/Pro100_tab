aa4350cdea6d03a02c67e9acc4afd91cea81abe1
Nikif <nikiforov_d.1973@mail.ru>
2022-11-24 23:37:33 +0400

    docker_new

--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -76,15 +76,23 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'CRUD',
+#         'USER': 'postgres',
+#         'PASSWORD': '711325',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'CRUD',
-        'USER': 'postgres',
-        'PASSWORD': '711325',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
